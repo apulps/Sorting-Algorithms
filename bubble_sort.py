531c5388a20639c613c3752337866d1d64c1b18c
apulps <apinedaminon@gmail.com>
2020-09-20 14:24:36 +0200

    Add bubble algorithm

---
+++ bubble_sort.py
@@ -0,0 +1,15 @@
+def bubble_sort(array):
+    n = len(array)
+
+    for i in range(n):
+        flag = False
+
+        for j in range(0, n-i-1):
+            if array[j] > array[j+1]:
+                array[j], array[j+1] = array[j+1], array[j]
+                flag = True
+        
+        if flag == False:
+            break
+
+    return array
