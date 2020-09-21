946e96a7905ccc84864494161fc2c82a87f0e553
apulps <apinedaminon@gmail.com>
2020-09-21 14:33:19 +0200

    Add merge algorithm

---
+++ merge_sort.py
@@ -0,0 +1,38 @@
+def merge_sort(array):
+    """
+    Merge sort
+
+    Complexity -> O(n*log(n))
+    """
+    if len(array) > 1:
+        m = len(array) // 2
+        left = array[:m]
+        right = array[m:]
+
+        merge_sort(left)
+        merge_sort(right)
+
+        i = 0
+        j = 0
+        k = 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                array[k] = left[i]
+                i += 1
+            else:
+                array[k] = right[j]
+                j += 1
+            k += 1
+
+        while i < len(left):
+            array[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            array[k] = right[j]
+            j += 1
+            k += 1
+
+    return array
