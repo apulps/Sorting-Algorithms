85ee4c7e03b5c5745bd154a7522358d65481f6f6
apulps <apinedaminon@gmail.com>
2020-09-21 13:58:18 +0200

    Add insertion algorithm

---
+++ insertion_sort.py
@@ -0,0 +1,14 @@
+def insertion_sort(array):
+    n = len(array)
+
+    for i in range(1, n):
+        value = array[i]
+        j = i - 1
+
+        while j >= 0 and value < array[j]:
+            array[j + 1] = array[j]
+            j -= 1
+
+        array[j + 1] = value 
+    
+    return array
