7494bed770d16fd3022803ecc5fd0a0a3f0c383b
apulps <apinedaminon@gmail.com>
2020-10-03 17:41:23 +0200

    Add insertion sort import to bucket algorithm

--- bucket_sort.py
+++ bucket_sort.py
@@ -1,23 +1,5 @@
-def insertion_sort(array):
-    """
-    Insertion sort
+from insertion_sort import insertion_sort
 
-    Complexity -> O(n^2)
-    """
-    n = len(array)
-
-    for i in range(1, n):
-        value = array[i]
-        j = i - 1
-
-        while j >= 0 and value < array[j]:
-            array[j + 1] = array[j]
-            j -= 1
-
-        array[j + 1] = value 
-    
-    return array
-    
 
 def bucket_sort(n):
     """
